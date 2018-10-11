c85786b21d427aa7e29d06978d51fd5ee197cbe3
Nicholas Crofts <nicholas.crofts@gmail.com>
2018-10-11 15:46:15 -0400

    delete pioneer sort of works

--- client/src/components/PioneerList.js
+++ client/src/components/PioneerList.js
@@ -23,7 +23,7 @@ export default class PioneerList extends Component {
     }
 
     handleChange = (event) => {
-        const newPioneer = {...this.state.newPioneer}
+        const newPioneer = { ...this.state.newPioneer }
         newPioneer[event.target.name] = event.target.value
         this.setState({ newPioneer })
     }
@@ -36,44 +36,42 @@ export default class PioneerList extends Component {
         this.setState({ pioneers })
     }
 
-//////////////// H A N D L E   D E L E T E //////////////
+    //////////////// H A N D L E   D E L E T E //////////////
 
     handleDelete = async (pioneerId) => {
         console.log('initiating delete')
         const response = await axios.delete(`/api/pioneers/${pioneerId}`)
-        this.setState({ pioneers: response.data })
-      }
+        this.setState({ pioneer: response.data })
+    }
 
-/////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////
 
     render() {
         const pioneersList = this.state.pioneers.map((pioneer, i) => {
-            return (<div key={i}>
-            <StyledLink to={`/pioneers/${pioneer._id}`} >{pioneer.pioneerName}</StyledLink>
-
-
-                <button onClick={() => this.handleDelete(this.state.pioneers._id)}>delete</button>
-
-            
-            </div>)
+            return (
+                <div key={i}>
+                    <StyledLink to={`/pioneers/${pioneer._id}`} >{pioneer.pioneerName}</StyledLink>
+                    <button onClick={() => this.handleDelete(pioneer._id)}>delete</button>
+                </div>
+            )
         })
 
-    return (
-      <div>
-         {pioneersList}
-         <form onSubmit={this.handleSubmit}>
-          <input 
-          type='text'
-          name='pioneerName'
-          placeholder='e n t e r   n a m e   h e r e'
-          value={this.state.newPioneer.pioneerName}
-          onChange={this.handleChange}
-          />
-          <input type='submit' value='Create New Pioneer'/>
-        </form>
-      </div>
-    )
-  }
+        return (
+            <div>
+                {pioneersList}
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type='text'
+                        name='pioneerName'
+                        placeholder='e n t e r   n a m e   h e r e'
+                        value={this.state.newPioneer.pioneerName}
+                        onChange={this.handleChange}
+                    />
+                    <input type='submit' value='Create New Pioneer' />
+                </form>
+            </div>
+        )
+    }
 }
 
 
